99c9650e16ea470b4b416dff4d233578c53c213c
kanadskiy <sammarrello@gmail.com>
2022-11-01 19:36:43 -0400

    included file in main and fixed css link

--- include.php
+++ include.php
@@ -7,7 +7,7 @@ function WriteHeaders($Heading = "", $Titlebar = "") {
         <head>
             <meta charset = \"UTF-8\">
             <title>$Titlebar</title>
-            <link rel=\"stylesheet\" type = \"text/css\" href=\"asstStyle.css\">
+            <link rel=\"stylesheet\" type = \"text/css\" href=\"style.css\">
         </head>
     <body>
     <h1>$Heading   
