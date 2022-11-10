7292a77462acc78d4dc150da13ce4c7b1be33052
Fidy34-rgb <113130124+Fidy34-rgb@users.noreply.github.com>
2022-11-10 11:18:28 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,4 +1,13 @@
 <?php 
 require_once("include.php");
-
+//main
+$mysqlObj = CreateConnectionObject();
+$TableName = "fontNames";
+//write headers with all student first and last names
+//if (isset($_POST['f_Save']))saveFile($p));
+// else if openFile();
+//  else if findButton, findTextInFile($p1, $p2);
+//else drawMenu();
+if (isset($mysqlObj)) $mysqlObj->close();
+//write footers
 ?>
